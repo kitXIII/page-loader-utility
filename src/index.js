0adab9a357c9badca4be60f6bb4e3f4d6603dd3d
Nikita <mipo4t@gmail.com>
2018-09-04 22:58:40 +0400

    refactor by remarks

--- src/index.js
+++ src/index.js
@@ -4,32 +4,22 @@ import path from 'path';
 
 const getFileName = url => `${url.match(/(?<=https?:\/\/).+/i)[0].replace(/\W/g, '-')}.html`;
 
-const ifDefine = arg => new Promise((resolve, reject) => {
-  if (arg === undefined) {
-    reject();
-  }
-  resolve(arg);
-});
-
-const isNotError = error => new Promise((resolve, reject) => {
-  if (error === undefined) {
-    resolve();
-  }
-  reject(error);
-});
-
-const makeDir = dirPath => fsPromises.stat(dirPath)
-  .then((stat) => {
-    if (stat.isFile()) {
-      throw new Error(`On path ${dirPath} there is a file`);
-    }
-    return true;
-  }, (error) => {
-    if (error.code === 'ENOENT') {
-      return fsPromises.mkdir(dirPath);
-    }
-    throw error;
-  });
+const makeDir = dirPath => (dirPath === undefined
+  ? Promise.resolve()
+  : fsPromises.stat(dirPath)
+    .then((stat) => {
+      if (stat.isFile()) {
+        throw new Error(`On path ${dirPath} there is a file`);
+      }
+      return true;
+    })
+    .catch((error) => {
+      if (error.code === 'ENOENT') {
+        return fsPromises.mkdir(dirPath);
+      }
+      throw error;
+    })
+);
 
 export default (url, outputDir, requester = axios) => requester.get(url)
   .then((response) => {
@@ -37,10 +27,10 @@ export default (url, outputDir, requester = axios) => requester.get(url)
       throw new Error(`Expected 200, but was ${response.status} for '${url}'`);
     }
     return response.data;
-  }).then((data) => {
+  })
+  .then((data) => {
     const fileName = getFileName(url);
     const outputPath = outputDir ? path.join(outputDir, fileName) : fileName;
-    return ifDefine(outputDir)
-      .then(makeDir, isNotError)
+    return makeDir(outputDir)
       .then(() => fsPromises.writeFile(outputPath, data, { encoding: 'utf8', mode: 0o777, flag: 'a+' }));
   });
