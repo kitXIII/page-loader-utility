8b828d75338e6e9a4d33f3930fef6ca98510eaba
Nikita <mipo4t@gmail.com>
2018-09-05 15:51:25 +0400

    refacot on remarks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { promises as fsPromises } from 'fs';
 import path from 'path';
-import cheerio from 'cheerio';
-import _ from 'lodash';
+// import cheerio from 'cheerio';
+// import _ from 'lodash';
 
 const getFileName = url => `${url.match(/(?<=https?:\/\/).+/i)[0].replace(/\W/g, '-')}.html`;
 
