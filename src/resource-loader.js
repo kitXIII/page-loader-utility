68741d7dcb52e0c3990a8c0e3ff6317949c51360
Nikita <mipo4t@gmail.com>
2018-09-08 00:40:23 +0400

    refactor

--- src/resource-loader.js
+++ src/resource-loader.js
@@ -13,53 +13,56 @@ const getNameByPathname = (pathname) => {
   return _.concat(dirParts, base).join('-');
 };
 
-const resources = [
+const tags = [
   {
-    tagName: 'script',
+    tag: 'script',
     srcAttr: 'src',
     responseType: 'text',
-    save: (data, filePath) => fsPromises.writeFile(filePath, data, 'utf8'),
+    writeFileProcess: (data, filePath) => fsPromises.writeFile(filePath, data, 'utf8'),
   },
   {
-    tagName: 'img',
+    tag: 'img',
     srcAttr: 'src',
     responseType: 'arraybuffer',
-    save: (data, filePath) => {
+    writeFileProcess: (data, filePath) => {
       const binaryData = Buffer.from(data);
       return fsPromises.writeFile(filePath, binaryData);
     },
   },
   {
-    tagName: 'link',
+    tag: 'link',
     srcAttr: 'href',
     responseType: 'text',
-    save: (data, filePath) => fsPromises.writeFile(filePath, data, 'utf8'),
+    writeFileProcess: (data, filePath) => fsPromises.writeFile(filePath, data, 'utf8'),
   },
 ];
 
+const getTagObject = tag => tags.find(item => tag === item.tag);
+
 const getLocalResoucesLinks = (page) => {
   const $ = cheerio.load(page);
-  const result = resources.map((tag) => {
-    const elements = $(tag.tagName).map((i, elem) => $(elem).attr(tag.srcAttr));
-    const links = Array.from(elements).filter(item => item.search('://') === -1);
+  const result = tags.map(({ tag, srcAttr }) => {
+    const collection = $(tag).map((i, elem) => $(elem).attr(srcAttr));
+    const links = Array.from(collection).filter(link => !url.parse(link).host);
     const resultLinks = _.uniq(links);
-    return resultLinks.map(pathname => ({ pathname, ...tag }));
+    return resultLinks.map(pathname => ({ pathname, tag }));
   });
   return _.flatten(result);
 };
 
 const changeLocalResourcesLinks = (page, links, outputPath) => {
   const $ = cheerio.load(page);
-  links.forEach((link) => {
-    const filePath = path.resolve(outputPath, getNameByPathname(link.pathname));
+  links.forEach(({ pathname, tag }) => {
+    const { srcAttr } = getTagObject(tag);
+    const filePath = path.resolve(outputPath, getNameByPathname(pathname));
     const baseDirPath = path.resolve(outputPath, '..');
     const newPath = path.relative(baseDirPath, filePath);
-    return $(`${link.tagName}[${link.srcAttr} = "${link.pathname}"]`).attr(link.srcAttr, newPath);
+    return $(`${tag}[${srcAttr} = "${pathname}"]`).attr(srcAttr, newPath);
   });
   return $.html();
 };
 
-const makeDir = dirPath => Promise.resolve(log(`Check, is dir exists ${dirPath}`))
+const makeDir = dirPath => Promise.resolve(log(`Check if dir exists ${dirPath}`))
   .then(() => fsPromises.readdir(dirPath))
   .catch((error) => {
     if (error.code === 'ENOENT') {
@@ -73,48 +76,52 @@ const makeDir = dirPath => Promise.resolve(log(`Check, is dir exists ${dirPath}`
     return null;
   });
 
-const loadResource = (uri, link, outputPath, loader) => Promise.resolve(log(`Try to load resource ${link.pathname}`))
-  .then(() => loader.get(url.resolve(uri, link.pathname), { responseType: link.responseType }))
-  .then((response) => {
-    log(`Received a response with status ${response.status}`);
-    const savePath = path.resolve(outputPath, getNameByPathname(link.pathname));
-    log(`Try to save received resource on ${savePath}`);
-    return link.save(response.data, savePath);
-  })
-  .then(() => {
-    log(`Resource ${link.pathname} was saved localy`);
-    return link;
-  });
+const loadResource = (uri, link, outputPath, loader) => {
+  const { tag, pathname } = link;
+  const { responseType, writeFileProcess } = getTagObject(tag);
+  log(`Try to load resource ${pathname}`);
+  return loader.get(url.resolve(uri, pathname), { responseType })
+    .then((response) => {
+      log(`Received a response with status ${response.status}`);
+      return response.data;
+    })
+    .then((data) => {
+      const filePath = path.resolve(outputPath, getNameByPathname(pathname));
+      log(`Try to save received resource to ${filePath}`);
+      return writeFileProcess(data, filePath);
+    })
+    .then(() => {
+      log(`Resource ${pathname} was saved localy`);
+      return link;
+    });
+};
 
-export default (uri, outputPath, page, loader) => Promise.resolve(log(`Try to load resources of page ${uri}`))
-  .then(() => {
-    log('Try to get local resources links');
-    const links = getLocalResoucesLinks(page);
-    const count = links.length;
-    log(`Links to resources are received in quantity: ${count}`);
-    if (count === 0) {
-      throw new Error('NO LINKS');
-    }
-    return links;
-  })
-  .then(links => Promise.all([
+export default (uri, outputPath, page, loader) => {
+  log(`Try to load resources of page ${uri}`);
+  log('Try to get local resources links');
+  const links = getLocalResoucesLinks(page);
+  const count = links.length;
+  log(`Links to resources are received in quantity: ${count}`);
+
+  if (count === 0) {
+    log('No resources to download');
+    return page;
+  }
+
+  const handlers = [
     makeDir(outputPath),
     Promise.all(links.map(link => loadResource(uri, link, outputPath, loader))),
-  ]))
-  .then(([, links]) => {
-    log('Begin to change local resources links');
-    const changedPage = changeLocalResourcesLinks(page, links, outputPath);
-    log(`Links to resources of page ${uri} was changed`);
-    return changedPage;
-  })
-  .then((html) => {
-    log('SUCCESS');
-    return html;
-  })
-  .catch((error) => {
-    if (error.message === 'NO LINKS') {
-      log('No links to load, return page');
-      return page;
-    }
-    throw error;
-  });
+  ];
+
+  return Promise.all(handlers)
+    .then(([, loadedLinks]) => {
+      log('Begin to change local resources links');
+      const changedPage = changeLocalResourcesLinks(page, loadedLinks, outputPath);
+      log(`Links to resources of page ${uri} was changed`);
+      return changedPage;
+    })
+    .then((html) => {
+      log('SUCCESS');
+      return html;
+    });
+};
