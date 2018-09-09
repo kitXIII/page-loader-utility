c0a77e997841d6e5c945d22f2f662b5558aecf2c
Nikita <mipo4t@gmail.com>
2018-09-10 02:42:22 +0400

    add lisr concurrent option

--- src/resources/batch-loader.js
+++ src/resources/batch-loader.js
@@ -1,13 +1,13 @@
 import Listr from 'listr';
 
-const listrBatchLoader = (links, oneLinkLoader, ...args) => Promise.all(links
-  .map((link) => {
-    const task = new Listr([{
-      title: `${link.uri}`,
-      task: () => oneLinkLoader(link, ...args),
-    }]);
-    return task.run();
-  }));
+const listrBatchLoader = (links, oneLinkLoader, ...args) => {
+  const tasks = new Listr(links.map(link => ({
+    title: `${link.uri}`,
+    task: () => oneLinkLoader(link, ...args),
+  })), { concurrent: true });
+
+  return tasks.run();
+};
 
 const promiseAllBatchLoader = (links, oneLinkLoader, ...args) => Promise.all(links
   .map(link => oneLinkLoader(link, ...args)));
